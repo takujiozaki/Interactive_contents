2d2c93abcaef4a046e9e394a4fa87457df1da4ac
takujiozaki <takujiozaki@gmail.com>
2020-06-18 07:26:01 +0900

    first commit

---
+++ Loop/script/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    courses.forEach(function(course){
+        assert("ck", course);
+        //$("#major").append("<option value=\""+course+"\">"+course+"</option>");
+        $("#major").append($("<option>").text(course).val(course));
+    });
+});
+
+//チェック関数
+const assert = function(msg, val){
+    console.log(msg + ":" + val);
+}
+
+const courses = ["Web専攻","イラスト専攻","グラフィック専攻"];
