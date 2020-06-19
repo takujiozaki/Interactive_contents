efd195f2142c1fe6adad3579d2f2b86409cd3f29
takujiozaki <takujiozaki@gmail.com>
2020-06-19 23:23:30 +0900

    shop

---
+++ Shop/scripts/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    assert('動作テスト', 'OK');
+
+    data.forEach(function(dounuts){
+        const str = `<tr><td><img src="images/${dounuts.picture}" height="100"></td><td>${recommendStar(dounuts.recommend)}</td><td>${dounuts.item}</td><td>${dounuts.price}円 ${showLimited(dounuts.new)}</td></tr>`
+        $('#showcase').append(str);
+    })
+});
+
+const data = [
+    {item:'リンゴジャム', price:120, picture:'01.jpg', recommend:5, new: true},
+    {item:'シュガー', price:100, picture:'02.jpg', recommend:4, new: false},
+    {item:'ファンシー', price:130, picture:'03.jpg', recommend:3, new: false}
+];
+
+const assert = function(msg, val){
+    console.log(`${msg} : ${val}`);
+}
+
+const recommendStar = function(rank){
+    let stars = "";
+    for(let i = 0; i < rank; i++){
+        stars += "★";
+    }
+    return stars;
+}
+
+const showLimited = function(limited){
+    let str = ""
+    if(limited){   
+        str = `<span class="badge badge-danger">新製品</span>`;
+    }
+    return str;
+}
